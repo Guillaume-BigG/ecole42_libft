3a273cb87c76017eb47ad590441b29d6cce95dca
Guillaume-BigG <guillaume.beaudoin96@gmail.com>
2023-11-06 21:14:48 +0100

    added ft_strlen.c

---
+++ ft_strlen.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guillaumebeaudoin <guillaumebeaudoin@st    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 21:01:11 by guillaumebe       #+#    #+#             */
+/*   Updated: 2023/11/06 21:14:12 by guillaumebe      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	main(void)
+{
+	char	source[12] = " ";
+
+	printf("%lu\n", strlen(source));
+	printf("%i\n", ft_strlen(source));
+}
+
