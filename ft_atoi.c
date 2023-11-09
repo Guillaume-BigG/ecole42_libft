6d08df19ed7a08e62cc8a0b85c3040c95efe8fa1
Guillaume-BigG <guillaume.beaudoin96@gmail.com>
2023-11-09 17:05:26 +0100

    added ft_atoi.c

---
+++ ft_atoi.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guillaumebeaudoin <guillaumebeaudoin@st    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 16:33:50 by guillaumebe       #+#    #+#             */
+/*   Updated: 2023/11/09 16:59:38 by guillaumebe      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_isdigit.c"
+
+int	ft_isdigit(int c);
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	nb;
+
+	i = 0;
+	sign = 1;
+	nb = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		nb = nb * 10 + (str[i] - 48);
+		i++;
+	}
+	return (nb * sign);
+}
+
+int	main(void)
+{
+	const char *str = "12345";
+	int number = atoi(str);
+	printf("Converted integer: %d\n", number);
+	return (0);
+}
