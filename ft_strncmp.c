9ab529555b13ab4855c7a5edd17ed0b85594166f
Guillaume-BigG <guillaume.beaudoin96@gmail.com>
2023-11-09 15:45:34 +0100

    added ft_strncmp.c

---
+++ ft_strncmp.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strncmp.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guillaumebeaudoin <guillaumebeaudoin@st    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 15:34:27 by guillaumebe       #+#    #+#             */
+/*   Updated: 2023/11/09 15:44:12 by guillaumebe      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	char			first[] = "maman";
+	char			second[] = "mab";
+	unsigned int	n;
+
+	n = 4;
+	printf("%d\n", ft_strncmp(first, second, n));
+	printf("%d\n", strncmp(first, second, n));
+}
